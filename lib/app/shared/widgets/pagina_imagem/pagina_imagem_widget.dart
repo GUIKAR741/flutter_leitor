1fe1760c4c9bf8bb733494ebf1b0c8c23ef64d7c
Guilherme Nepomuceno de Carvalho <guilhermenepomuceno46@gmail.com>
2022-08-26 17:31:14 -0300

    melhoria visualização pela orientação

--- lib/app/shared/widgets/pagina_imagem/pagina_imagem_widget.dart
+++ lib/app/shared/widgets/pagina_imagem/pagina_imagem_widget.dart
@@ -13,45 +13,50 @@ class PaginaImagemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ExtendedImage.network(
-      url,
-      fit: BoxFit.contain,
-      cache: true,
-      mode: ExtendedImageMode.gesture,
-      initGestureConfigHandler: (state) {
-        return GestureConfig(
-          minScale: 0.9,
-          animationMinScale: 0.7,
-          maxScale: 3.0,
-          animationMaxScale: 3.5,
-          speed: 1.0,
-          inertialSpeed: 100.0,
-          initialScale: 1.0,
-          inPageView: false,
-          initialAlignment: InitialAlignment.center,
-        );
-      },
-      loadStateChanged: (ExtendedImageState state) {
-        switch (state.extendedImageLoadState) {
-          case LoadState.loading:
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          case LoadState.completed:
-            return state.completedWidget;
-          case LoadState.failed:
-            Modular.get<FirebaseCrashlytics>()
-                .log('Erro ao Carregar Imagem $url');
-            return Center(
-              child: ElevatedButton(
-                onPressed: state.reLoadImage,
-                child: const Text('Erro ao carregar'),
-              ),
-            );
-          default:
-            return null;
-        }
-      },
+    return OrientationBuilder(
+      builder: (_, o) => ExtendedImage.network(
+        url,
+        fit: BoxFit.contain,
+        cache: true,
+        filterQuality: FilterQuality.high,
+        mode: ExtendedImageMode.gesture,
+        initGestureConfigHandler: (state) {
+          return GestureConfig(
+            minScale: 0.9,
+            animationMinScale: 0.7,
+            maxScale: 3.0,
+            animationMaxScale: 3.5,
+            speed: 1.0,
+            inertialSpeed: 100.0,
+            initialScale: o == Orientation.portrait ? 1.0 : 3.0,
+            inPageView: false,
+            initialAlignment: o == Orientation.portrait
+                ? InitialAlignment.center
+                : InitialAlignment.topCenter,
+          );
+        },
+        loadStateChanged: (ExtendedImageState state) {
+          switch (state.extendedImageLoadState) {
+            case LoadState.loading:
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            case LoadState.completed:
+              return state.completedWidget;
+            case LoadState.failed:
+              Modular.get<FirebaseCrashlytics>()
+                  .log('Erro ao Carregar Imagem $url');
+              return Center(
+                child: ElevatedButton(
+                  onPressed: state.reLoadImage,
+                  child: const Text('Erro ao carregar'),
+                ),
+              );
+            default:
+              return null;
+          }
+        },
+      ),
     );
   }
 }
